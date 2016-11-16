085598e053a476e90f1a6865f55412b53e680219
ewgenius <ewgeniux@gmail.com>
2016-11-16 09:52:15 +0300

    pubnub testing

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import '../../lib/pubnub'
 
 const mapProps = state => ({
 
---
+++ src/lib/pubnub.js
@@ -0,0 +1,26 @@
+//import PubNub from 'pubnub'
+
+const pubnub = PUBNUB.init({
+  subscribeKey: 'sub-c-62905d82-abc1-11e6-80fa-02ee2ddab7fe',
+  //publishKey: 'pub-c-7449096e-dc32-4c86-879e-c1c00c0425ba',
+  //ssl: (location.protocol.toLowerCase() === 'https:')
+})
+
+pubnub.subscribe({
+  channel: 'pubnub-twitter',
+  callback: msg => {
+    console.log(msg)
+  }
+})
+
+pubnub.addListener({
+  message(message) {
+    console.log('MESSSAGE:', message)
+  },
+  presence(presence) {
+    console.log('PRESENCE', presence)
+  },
+  status(status) {
+    console.log('STATUS:', status)
+  }
+})
